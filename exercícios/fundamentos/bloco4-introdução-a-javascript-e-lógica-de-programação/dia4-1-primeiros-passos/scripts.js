46dd87a383dc9da6a66f44338ddfb9deda2aee6b
Isabelle Strojake <strojake@gmail.com>
2022-04-06 17:17:45 -0300

    operadores lógicos

--- exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-1-primeiros-passos/scripts.js
+++ exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-1-primeiros-passos/scripts.js
@@ -1,11 +1,32 @@
-const nota = 93;
+const hour = 17;
+message = "";
 
-if (nota >= 80) {
-  console.log("Parabéns, você foi aprovado(a)!");
+if (hour >= 22) {
+  message = "Não deveríamos comer nada, é hora de dormir";
 }
-else if (nota < 80 && nota >= 60) {
-  console.log("Você está em nossa lista de espera.");
+else if (hour >= 18 && hour < 20) {
+  message = "Rango da noite, vamos jantar :D";
+}
+else if (hour >= 14 && hour < 18) {
+  message = "Vamos fazer um bolo pro café da tarde?";
+}
+else if (hour >= 11 && hour < 14) {
+  message = "Hora do almoço!!!";
+}
+else if (hour >= 4 && hour < 11) {
+  message = "Hmmm, cheiro de café recém passado";
 }
 else {
-  console.log("Você foi reprovado(a).");
+  message = "Você está dormindo?";
+}
+
+console.log(message);
+
+const weekDay = "domingo";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else {
+  console.log("FINALMENTE, descanso merecido UwU");
 }
