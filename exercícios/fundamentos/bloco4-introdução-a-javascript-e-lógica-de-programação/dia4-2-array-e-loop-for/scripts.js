1fabbb2a51e2e80a932a96ad3a58f4d72c53210b
Isabelle Strojake <strojake@gmail.com>
2022-04-07 15:24:42 -0300

    arrays

---
+++ exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-2-array-e-loop-for/scripts.js
@@ -0,0 +1,13 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let menuServices = menu.indexOf('Serviços');
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+menu.push('Contato')
+
+console.log(menuServices);
+
+console.log(indexOfPortfolio);
+
+console.log(menu)
