4c60aab96c757f86e16fad0798c3de82101c3c95
Isabelle Strojake <strojake@gmail.com>
2022-04-08 15:37:27 -0300

    exercício 3

--- exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-3-lógica-de-programação-e-algoritmos/scripts.js
+++ exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-3-lógica-de-programação-e-algoritmos/scripts.js
@@ -14,3 +14,24 @@ for (let index = 0; index < palavra.length; index += 1) {
     palavraInversa += palavra[palavra.length - 1 - index]
 }
 console.log(palavraInversa)
+
+<!-- Retornar a maior ou menor palavra -->
+let aprendizados = ['Java', 'JavaScript', 'Python', 'HTML', 'CSS'];
+
+let maiorPalavra = aprendizados[0];
+let menorPalavra = aprendizados[0];
+
+for (let index = 0; index < aprendizados.length; index+= 1) {
+    if (aprendizados[index].length > maiorPalavra.length){
+        maiorPalavra = aprendizados[index]
+    }
+}
+
+for (let index = 0; index < aprendizados.length; index+= 1) {
+    if (aprendizados[index].length < menorPalavra.length){
+        menoralavra = aprendizados[index]
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra)
