08df5082188c4198a087e51615ffb5b7397bbbf8
Isabelle Strojake <strojake@gmail.com>
2022-04-08 15:30:56 -0300

    exercício 2

--- exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-3-lógica-de-programação-e-algoritmos/scripts.js
+++ exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-3-lógica-de-programação-e-algoritmos/scripts.js
@@ -1,6 +1,16 @@
+<!-- Fatorial de 10 -->
 let fatorial = 1;
 
 for (let index = 10; index > 0; index -= 1) {
     fatorial *= index;
 }
 console.log(fatorial)
+
+<!-- Inverter a palavra -->
+let palavra = 'Tryber';
+let palavraInversa = "";
+
+for (let index = 0; index < palavra.length; index += 1) {
+    palavraInversa += palavra[palavra.length - 1 - index]
+}
+console.log(palavraInversa)
