1c9da07cf738f5aeab824d54abccf87be7557305
Isabelle Strojake <strojake@gmail.com>
2022-04-11 15:47:56 -0300

    for in e for of

--- exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-4-objetos-e-funções/scripts.js
+++ exercícios/fundamentos/bloco4-introdução-a-javascript-e-lógica-de-programação/dia4-4-objetos-e-funções/scripts.js
@@ -15,3 +15,23 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.')
 
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' melhadas de prata.')
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+for (let person in names){
+    console.log('Olá, ' + names[person]+ '.')
+}
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for (let index in car) {
+    console.log(index, car[index]);
+}
