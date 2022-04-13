ba5af8b3f9fff3825b6494a9943f851c7316c6ac
Isabelle Strojake <strojake@gmail.com>
2022-04-13 12:15:57 -0300

    parte 3

--- exercícios/fundamentos/bloco5-javascript-dom-eventos-e-web-storage/5-1-dom-e-seletores/scripts.js
+++ exercícios/fundamentos/bloco5-javascript-dom-eventos-e-web-storage/5-1-dom-e-seletores/scripts.js
@@ -1,1 +1,21 @@
-console.log(document.getElementById("page-title");)
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'lightgreen';
+
+const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+emergencyTasks.style.backgroundColor = 'violet';
+
+const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+noEmergencyTasks.style.backgroundColor = 'darkgrey';
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'darkgreen';
+
+const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+  emergencyTasksHeaders[index].style.backgroundColor = 'rgb(125, 34, 223)';
+}
+
+const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+  noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(28, 29, 37)';
+}
