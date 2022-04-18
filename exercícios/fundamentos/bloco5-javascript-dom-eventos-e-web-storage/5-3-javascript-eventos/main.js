21fd1263191fb48f899300792684648a77fa2cb5
Isabelle Strojake <strojake@gmail.com>
2022-04-18 17:25:32 -0300

    prática 1 e 2

--- exercícios/fundamentos/bloco5-javascript-dom-eventos-e-web-storage/5-3-javascript-eventos/main.js
+++ exercícios/fundamentos/bloco5-javascript-dom-eventos-e-web-storage/5-3-javascript-eventos/main.js
@@ -1,62 +1,59 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
-const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
 
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
 
-// 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-// Resposta(Ise): Foi-lhe atribuído um código em CSS que muda sua posição verticalmente. Falando de um ranking, dá destaque ao primeiro lugar.
+    weekDaysList.appendChild(dayListItem);
+  };
+};
 
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-  function changingTech(event){
-    const elementTech = document.querySelector('.tech');
-    elementTech.classList.remove('tech');
-    event.target.classList.add('tech');
-    input.value = '';
-  }
-  firstLi.addEventListener('click', changingTech);
-  secondLi.addEventListener('click', changingTech);
-  thirdLi.addEventListener('click', changingTech);
+createDaysOfTheWeek();
 
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-  input.addEventListener('input', function(event) {
-    const elementTech = document.querySelector('.tech');
-    elementTech.innerText = event.target.value;
-  });
+// Escreva seu código abaixo.
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portfólio?
-  myWebpage.addEventListener('dblclick', function() {
-    window.location.replace('https://isabellestrojake.github.io./');
-  });
+// 1 //
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-  myWebpage.addEventListener('mouseover', function(event) {
-    event.target.style.color = '#FB4376';
-  });
-  myWebpage.addEventListener('mouseout', function(event) {
-    event.target.style.color = 'unset';
-  });
+  function createDaysOfMonth() {
+    let getDaysList = document.querySelector('#days');
 
-// Segue abaixo um exemplo do uso de event.target:
+    for (let index = 0; index < dezDaysList.length; index += 1){
+      let day = dezDaysList[index];
+      let dayItem = document.createElement('li');
 
+      if (day === 24 || day === 31) {
+        dayItem.className = 'Holiday';
+        dayItem.innerHTML = day;
+        getDaysList.appendChild(dayItem);
+      } else if (day === 4 || day === 11 || day === 18) {
+        dayItem.className = 'Friday-day';
+        dayItem.innerHTML = day;
+        getDaysList.appendChild(dayItem);
+      } else if (day === 25) {
+        dayItem.className = 'Holiday Friday-day';
+        dayItem.innerHTML = day;
+        getDaysList.appendChild(dayItem);
+      } else {
+        dayItem.className = 'Day';
+        dayItem.innerHTML = day;
+        getDaysList.appendChild(dayItem);
+      }
+      }
+    }
+  createDaysOfMonth();
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
+// 2 //
+    function createHolidayButton(buttonHoliday) {
+      let buttonContainer = document.querySelector('.buttons-container');
+      let newButton = document.createElement('button');
+      let newButtonID = 'btn-holiday';
 
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+      newButton.innerHTML = buttonHoliday;
+      newButton.id = newButtonID;
+      buttonContainer.appendChild(newButton);
+    }
+  createHolidayButton('Feriados');
