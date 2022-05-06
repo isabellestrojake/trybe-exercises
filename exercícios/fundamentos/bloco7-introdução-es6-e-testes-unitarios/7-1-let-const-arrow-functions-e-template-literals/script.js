2c3beaa396526cb5c401860ba82f114b11d94501
Isabelle Strojake <strojake@gmail.com>
2022-05-06 14:14:06 -0300

    parte 2

--- exercícios/fundamentos/bloco7-introdução-es6-e-testes-unitarios/7-1-let-const-arrow-functions-e-template-literals/script.js
+++ exercícios/fundamentos/bloco7-introdução-es6-e-testes-unitarios/7-1-let-const-arrow-functions-e-template-literals/script.js
@@ -1,31 +1,35 @@
-/*const testingScope = escopo => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  }else {
-    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
+/*const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+console.log(factorial(11));*/
+
+/*const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));*/
+
+/*const array = ["CSS", "HTML", "JS"];
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    );
+
+    let result = `${fun1(paramOne)}
+
+    Minhas três principais habilidades são:`;
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+    ${result}
+
+    #goTrybe
+    `;
+
+    return result;
 }
-testingScope(true);*/
 
-/*const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-const sortOddAndEvens = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
-
-  return oddsAndEvens;
-}
-const sortedArray = sortOddAndEvens();
-console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente.`);*/
-
-/*const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);*/
-
+console.log(buildSkillsPhrase("Ise"));*/
