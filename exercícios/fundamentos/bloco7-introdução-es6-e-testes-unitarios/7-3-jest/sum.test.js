bffe3141ac425c1c293c5739df0e4e039faa4c34
Isabelle Strojake <strojake@gmail.com>
2022-05-10 19:24:28 -0300

    exercícios 1 e 2

---
+++ exercícios/fundamentos/bloco7-introdução-es6-e-testes-unitarios/7-3-jest/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('./sum');
+
+describe('Testa a função sum', () => {
+  test('deve retornar o resultado da soma', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+  test('deve disparar erro caso um valor seja uma string', () => {
+    expect(() => sum(4, '5')).toThrowError();
+    expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
+  });
+});
+
