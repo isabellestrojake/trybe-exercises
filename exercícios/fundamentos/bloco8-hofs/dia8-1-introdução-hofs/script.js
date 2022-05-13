95c20f75939c6b89216034491e57863e2c6dd8e0
Isabelle Strojake <strojake@gmail.com>
2022-05-13 16:16:04 -0300

    exercício 3

--- exercícios/fundamentos/bloco8-hofs/dia8-1-introdução-hofs/script.js
+++ exercícios/fundamentos/bloco8-hofs/dia8-1-introdução-hofs/script.js
@@ -13,11 +13,34 @@ const newEmployees = (callback) => {
 };
 console.log(newEmployees(employGenerator));*/
 
-const verificaNumero = (meuNumero, numero) => meuNumero === numero;
+/*const verificaNumero = (meuNumero, numero) => meuNumero === numero;
 
 const resultadoLoteria = (meuNumero, callback) => {
   const numero = Math.floor((Math.random() * 5) + 1);
 
   return callback(meuNumero, numero) ? 'Dia de sorte, você ganhou!' : 'Não foi dessa vez...';
 }
-console.log(resultadoLoteria(5, verificaNumero));
+console.log(resultadoLoteria(5, verificaNumero));*/
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compare = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  }
+  if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+}
+
+const pontos = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado Final: ${contador} pontos.`
+}
+console.log(pontos(RIGHT_ANSWERS, STUDENT_ANSWERS, compare));
